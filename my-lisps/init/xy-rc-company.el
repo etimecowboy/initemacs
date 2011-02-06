931fba24a276684c28c7c98ffeca90923ee16091
Xin Yang <Xin2.Yang@gmail.com>
2011-02-06 02:21:22 +0000

    Fix a small bug

--- my-lisps/init/xy-rc-company.el
+++ my-lisps/init/xy-rc-company.el
@@ -1,7 +1,7 @@
 ;;   -*- mode: emacs-lisp; coding: utf-8-unix  -*- 
 ;;--------------------------------------------------------------------
 ;; File name:    `xy-rc-company.el'
-;; Time-stamp:<2011-02-06 Sun 01:21 xin on p6t>
+;; Time-stamp:<2011-02-06 Sun 02:20 xin on p6t>
 ;; Author:       Xin Yang
 ;; Email:        xin2.yang@gmail.com
 ;; Depend on:    None
@@ -27,6 +27,6 @@
    `(c-mode-common-hook lisp-mode-hook emacs-lisp-mode-hook
      java-mode-hook lisp-interaction-mode-hook sh-mode-hook
      ,(if (not is-before-emacs-21) 'awk-mode-hook) ruby-mode-hook)
-   'company-mode)
+   'company-mode))
   
 (provide 'xy-rc-company.el)
