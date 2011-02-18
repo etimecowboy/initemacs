fbc76eeb23e0fe09a88542a8ce15165fd51e8a5b
Xin Yang <Xin2.Yang@gmail.com>
2011-02-18 16:35:40 +0000

    Some new files are added

---
+++ my-lisps/init/xy-rc-uniquify.el
@@ -0,0 +1,25 @@
+;;   -*- mode: emacs-lisp; coding: utf-8-unix  -*- 
+;;--------------------------------------------------------------------
+;; File name:    `xy-rc-uniquify.el'
+;; Time-stamp:<2011-02-18 Fri 16:14 xin on p6t>
+;; Author:       Xin Yang
+;; Email:        xin2.yang@gmail.com
+;; Depend on:    None
+;; Description:  My `uniquify.el' settings
+;;    ___ _ __ ___   __ _  ___ ___
+;;   / _ \ '_ ` _ \ / _` |/ __/ __|
+;;  |  __/ | | | | | (_| | (__\__ \
+;;  \____|_| |_| |_|\__,_|\___|___/
+;;
+;;--------------------------------------------------------------------
+
+(require 'cl)
+(require 'xy-rc-utils)
+
+;;;###autoload
+(defun uniquify-settings ()
+  "Settings of `uniquify.el'."
+
+  (setq uniquify-buffer-name-style 'forward))
+
+(provide 'xy-rc-uniquify.el)
