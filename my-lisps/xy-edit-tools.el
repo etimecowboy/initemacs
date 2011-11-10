02391940f720a1708939cd980a6c6a80b254ee02
Xin Yang <Xin2.Yang@gmail.com>
2011-11-10 14:42:04 +0000

    Add functions for viewing differences between the current buffer and the original file

--- my-lisps/xy-edit-tools.el
+++ my-lisps/xy-edit-tools.el
@@ -19,7 +19,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; 较早前收集的一些有用的辅助编辑函数
+;; 收集的一些有用的辅助编辑函数
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -312,6 +312,60 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
 		((looking-at "\\s\)") (forward-char 1) (backward-list 1))
 		(t (self-insert-command (or arg 1)))))
 ;; (global-set-key "%" 'match-paren)
+
+;;--------------------------------------------------------------------------------
+
+;; 存盘前查看改动了哪些地方，比较一下buffer里的内容与文件系统里老x文件
+;; 的不同
+;; REF: http://www.bookshelf.jp/soft/meadow_44.html#SEC661
+;;;###autoload
+(defun diff-with-original (ediff)
+  "Examin diff of current buffer with original file.
+        If with prefix, do interactive merge using `ediff-with-original'. "
+  (interactive "P")
+  (if ediff
+      (ediff-with-original)
+    ;; simple diff view with diff-mode
+    (require 'ediff)
+    (let ((diff-buf (get-buffer-create (format "*diff %s*" (buffer-file-name))))
+          (ediff-diff-options "-u") ;; is it your favourite?
+          (tmpfile (ediff-make-temp-file (current-buffer))))
+      (save-excursion
+        (set-buffer diff-buf)
+        (setq buffer-read-only nil)
+        (buffer-disable-undo)
+        (erase-buffer))
+      (ediff-make-diff2-buffer diff-buf
+                               (buffer-file-name)
+                               tmpfile)
+      (delete-file tmpfile)
+      (set-buffer diff-buf)
+      (if (< (buffer-size) 1)
+          (message "No differences with original file.")
+        (condition-case nil
+            (progn
+              (require 'diff-mode)
+              (diff-mode))
+          (error))
+        (goto-char 1)
+        (pop-to-buffer diff-buf)))))
+
+;;;###autoload
+(defun ediff-with-original ()
+  (interactive)
+  ;; interactive merge using ediff
+  (let ((file buffer-file-name)
+        (buf (current-buffer))
+        (orig-buf (get-buffer-create (concat "*orig " buffer-file-name "*"))))
+    (set-buffer orig-buf)
+    (setq buffer-read-only nil)
+    (buffer-disable-undo)
+    (erase-buffer)
+    (insert-file file)
+    (setq buffer-read-only t)
+    (set-buffer-modified-p nil)
+    (ediff-buffers orig-buf buf)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
