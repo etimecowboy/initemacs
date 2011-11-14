7ba47e6315a543925c81d26fdab7b7b4f663c0a6
Xin Yang <Xin2.Yang@gmail.com>
2011-11-14 08:30:27 +0000

    Minor changes

--- my-lisps/init/xy-rcroot-env.el
+++ my-lisps/init/xy-rcroot-env.el
@@ -1,7 +1,7 @@
 ;;   -*- mode: emacs-lisp; coding: utf-8-unix  -*- 
 ;;--------------------------------------------------------------------
 ;; File name:    `xy-rcroot-env.el'
-;; Time-stamp:<2011-11-14 Mon 08:15 xin on P6T-WIN7>
+;; Time-stamp:<2011-11-14 Mon 08:25 xin on P6T-WIN7>
 ;; Author:       Xin Yang
 ;; Email:        xin2.yang@gmail.com
 ;; Depend on:    None
@@ -122,11 +122,12 @@
            (- (+ hi lo) (+ (first *emacs-load-start*)
                            (second *emacs-load-start*)))))
 
-(xy/install-all-lisps (concat my-local-lisp-path "/w3m"))
-(message "* ---[ w3m lisps installed at %ds ]---"
-         (destructuring-bind (hi lo ms) (current-time)
-           (- (+ hi lo) (+ (first *emacs-load-start*)
-                           (second *emacs-load-start*)))))
+;; BUG: current cvs version has a bug when compiling
+;; (xy/install-all-lisps (concat my-local-lisp-path "/w3m"))
+;; (message "* ---[ w3m lisps installed at %ds ]---"
+;;          (destructuring-bind (hi lo ms) (current-time)
+;;            (- (+ hi lo) (+ (first *emacs-load-start*)
+;;                            (second *emacs-load-start*)))))
 
 (xy/install-all-lisps (concat my-local-lisp-path "/mew"))
 (message "* ---[ mew lisps installed at %ds ]---"
